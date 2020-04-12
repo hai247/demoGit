f95937eb824f019ce92b6066ef0b3b057132604b
HaiDo <dobahai007@gmail.com>
2020-04-12 15:18:44 +0700

    Add new file main.js

---
+++ main.js
@@ -0,0 +1,6 @@
+/**
+ * Khởi tạo git mới: git init
+ * Khai báo họ tên: git --global user.name "..."
+ * Khai báo email: git --global user.email ...
+ * Khai báo địa chỉ với server: git remote add origin
+ */
