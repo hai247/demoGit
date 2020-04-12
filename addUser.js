234413ec90b27bda0617bc5e48237461a177eb11
HaiDo <dobahai007@gmail.com>
2020-04-12 16:56:20 +0700

    Add new addUser

---
+++ addUser.js
@@ -0,0 +1,27 @@
+/**
+ * Khởi tạo git mới: git init
+ * Khai báo họ tên: git --global user.name "..."
+ * Khai báo email: git --global user.email ...
+ * Khai báo địa chỉ với server: git remote add origin ...
+ * Commit filt : git commit -m "..."
+ * Đẩy file lên gitHub : git push -u origin master
+ * 
+ * ==============================================================
+ * 
+ * Developer khác:
+ * Lấy folder từ gitHub (chỉ lấy lần đầu tiên): git clone "<link>""
+ * Trỏ đến folder trong máy tính: git cd "<link folder>""
+ * Đẩy lên server: git push
+ * Chia nhánh: git checkout -b <tên nhánh>
+ * Xem vị trí nhánh hiện tại: git branch (*note: dev lưa vào nhánh con; chỉ reviewer là người đc update vào nhánh master)
+ * Chuyển về nhánh master: git checkout master
+ */
+
+ /**
+ * git pull
+ * git chechout -b addUser
+ * git push --set-upstream origin addUser
+ * git add <file>
+ * git commit -m ""
+ * git push
+ */
